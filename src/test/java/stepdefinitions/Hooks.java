bc56ead160f723d0702832efc5a1bc1f6ddbe143
firdevs <ffirdevskaya@gmail.com>
2021-02-19 17:48:06 +0300

    ilk

---
+++ src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,36 @@
+package stepdefinitions;
+
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+public class Hooks {
+
+
+
+
+    //@Before TestNg'deki @BeforeMethod gibi calisir.
+    //Her scenario'dan once calisir
+    //GLOBAL Hooks annotation
+    //@Before
+    public void setUp(){
+
+
+        System.out.println("Hooks Class- Setup Method");}
+    //her scenario'dan sonra calisir
+    //@After
+    public void tearDown(Scenario scenario){
+
+
+        System.out.println("Hooks Class - tearDown Method");
+        //screenshot almak icin
+        final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+        Driver.closeDriver();
+    }
+
+}
+
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,76 @@
+package utilities;
+
+import com.google.common.base.Function;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.*;
+
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Driver {
+
+    private Driver() {
+
+    }
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (ConfigurationReader.getProperty("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ;
+                    driver = new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "ie":
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+                case "handless=chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+            }
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
